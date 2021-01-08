e335eff7739faab3f2ee3c8078448bb9201ceeda
SlumdogProgrammer <bolotin99@inbox.ru>
2021-01-08 16:38:12 +0300

    plug-in FeignClients

--- src/main/java/com/rate/exchange/gif/currencyservice/CurrencyServiceApplication.java
+++ src/main/java/com/rate/exchange/gif/currencyservice/CurrencyServiceApplication.java
@@ -2,7 +2,9 @@ package com.rate.exchange.gif.currencyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CurrencyServiceApplication {
 

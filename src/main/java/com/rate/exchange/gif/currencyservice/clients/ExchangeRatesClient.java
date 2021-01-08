f9b3f96c71af7ab513c21f67fbd1ab2a16e93c47
SlumdogProgrammer <bolotin99@inbox.ru>
2021-01-08 18:05:49 +0300

    delete useless brackets

--- src/main/java/com/rate/exchange/gif/currencyservice/clients/ExchangeRatesClient.java
+++ src/main/java/com/rate/exchange/gif/currencyservice/clients/ExchangeRatesClient.java
@@ -5,15 +5,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exchange_rates",url = "${openexchagerates.uri}")
+@FeignClient(name = "exchangeRates-client",url = "${openexchagerates.uri}")
 public interface ExchangeRatesClient {
 
-    @GetMapping("latest.json?app_id={app_id}&base={base}}&symbols={symbols}}")
+    @GetMapping("latest.json?app_id={app_id}&base={base}&symbols={symbols}")
     ExchangeRate getLatest(@PathVariable("app_id") String app_id,
                            @PathVariable("base") String base,
                            @PathVariable("symbols") String symbols);
 
-    @GetMapping("historical/{date}.json?app_id={app_id}&base={base}}&symbols={symbols}}")
+    @GetMapping("historical/{date}.json?app_id={app_id}&base={base}&symbols={symbols}")
     ExchangeRate getHistorical(@PathVariable("date") String date,
                                @PathVariable("app_id") String app_id,
                                @PathVariable("base") String base,

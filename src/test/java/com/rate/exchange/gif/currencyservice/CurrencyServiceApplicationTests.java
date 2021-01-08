d4bf3f24de49af565feb388bb74e7c0bf3f61a68
SlumdogProgrammer <bolotin99@inbox.ru>
2021-01-08 14:25:29 +0300

    create project

---
+++ src/test/java/com/rate/exchange/gif/currencyservice/CurrencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rate.exchange.gif.currencyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

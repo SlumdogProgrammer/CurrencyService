e3f34f8f8a7f03718a0a636e2cfa655417377558
SlumdogProgrammer <bolotin99@inbox.ru>
2021-01-14 21:42:01 +0300

    add tests

--- src/test/java/com/rate/exchange/gif/currencyservice/CurrencyServiceApplicationTests.java
+++ src/test/java/com/rate/exchange/gif/currencyservice/CurrencyServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.rate.exchange.gif.currencyservice;
 
+import com.rate.exchange.gif.currencyservice.services.ExchageRatesService;
+import com.rate.exchange.gif.currencyservice.services.GiphyService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.ArgumentMatchers.anyString;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CurrencyServiceApplicationTests {
 
+    @Autowired
+    private GiphyService gifService;
+    @MockBean
+    private ExchageRatesService gifClient;
+
     @Test
-    void contextLoads() {
+    public void whenPositiveChanges() {
+        boolean todayMore = true;
+        Mockito.when(gifClient.todayMore(anyString()))
+                .thenReturn(todayMore);
+        Object res = gifService.getGiphy(true);
+        Assert.assertNotNull(res);
     }
 
 }
